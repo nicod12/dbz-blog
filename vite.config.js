c18e0bbd60ce5a8ef5d3a889247f2db3c69a30c0
nicod12 <dumenicolas45@gmail.com>
2023-01-15 13:46:38 -0300

    updateII

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-import vsharp from "vite-plugin-vsharp"
 
 // https://vitejs.dev/config/
 export default defineConfig({
